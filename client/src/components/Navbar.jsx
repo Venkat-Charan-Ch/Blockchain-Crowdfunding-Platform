e1d53cf8a955754949461de6b086ac385df49020
Venkat-Charan-Ch <venkatcharanch@gmail.com>
2024-02-21 19:57:45 +0530

    finally working icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { CustomButton } from "./";
+import { logo, menu, search, thirdweb } from "../assets";
+import { navlinks } from "../constants";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div>Navbar</div>;
+};
 
-export default Navbar
+export default Navbar;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,5 @@
-export { default as Sidebar } from './Sidebar';
+export { default as Sidebar } from "./Sidebar";
 
-export { default as Navbar } from './Navbar';
+export { default as Navbar } from "./Navbar";
+
+export { default as CustomButton } from "./CustomButton";
